d88056e14c938a78e089706d104210433c9b1083
XiaKeMing <xiakeming97@gmail.com>
2023-06-25 14:10:30 +0800

    :rocket: chore(配置文件): 重新配置antd插件和依赖

--- config/antd.ts
+++ config/antd.ts
@@ -2,22 +2,22 @@ export default {
   // 配置提供程序
   configProvider: {},
   // 主题
-  dark: true,
-  compact: true,
+  dark: false,
+  // compact: false,
   // babel-plugin-import
-  import: true,
+  import: false,
   // 更少或 css，默认更少
-  style: "less",
+  // style: "less",
   // “configProvider.theme”的快捷方式
   // 用于配置主题令牌，仅限 antd v5
-  theme: {},
+  // theme: {},
   // antd <App /> 适用于版本 5.1.0 或更高版本，默认值：未定义
-  appConfig: {},
+  // appConfig: {},
   // 将 DayJS 转换为 MomentJS
   momentPicker: true,
   // 为旧版浏览器添加样式提供程序
-  styleProvider: {
-    hashPriority: "high",
-    legacyTransformer: true,
-  },
+  // styleProvider: {
+  //   hashPriority: "high",
+  //   legacyTransformer: true,
+  // },
 };
--- config/config.ts
+++ config/config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "umi";
 import routes from "./routes";
+import antd from './antd'
 
 export default defineConfig({
   routes,
   npmClient: "yarn",
   tailwindcss: {},
-  plugins: ["@umijs/plugins/dist/tailwindcss"],
+  plugins: ["@umijs/plugins/dist/tailwindcss","@umijs/plugins/dist/antd"],
+  antd,
 });
