e15c5d7f8774e8c27719773b428e6d68c7d96731
XiaKeMing <xiakeming97@gmail.com>
2023-08-23 19:21:00 +0800

    :package: build(pages/index.tsx): 测试打包上线

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
-import React, { useState } from 'react';
-import yayJpg from '../assets/yay.jpg';
-import { Button, Typography } from 'antd';
+/*
+ * @Author: XiaKeMing xiakeming97@gmail.com
+ * @Date: 2023-06-24 00:29:10
+ * @LastEditors: XiaKeMing xiakeming97@gmail.com
+ * @LastEditTime: 2023-08-23 19:18:49
+ * @FilePath: /beautiful-lyrics-h5/src/pages/index.tsx
+ * @Description:首页
+ *
+ * Copyright (c) 2023 by XIAKEMING, All Rights Reserved.
+ */
+import React, { useState } from "react";
+import yayJpg from "../assets/yay.jpg";
+import { Button, Card, Typography } from "antd";
 const { Paragraph, Text } = Typography;
 
 export default function HomePage() {
   const [ellipsis, setEllipsis] = useState(true);
   return (
     <div>
-      <Button>123</Button>
+      <Card>这是我的第一个网站</Card>
     </div>
   );
 }
