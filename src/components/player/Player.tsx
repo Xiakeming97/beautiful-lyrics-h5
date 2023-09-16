494b27afcb503c185930b16d3ccfcb317d909139
Xiakeming <xiakeming97@gmail.com>
2023-09-17 00:22:23 +0800

    :lipstick: style(player): 更新播放组件样式

--- src/components/player/Player.tsx
+++ src/components/player/Player.tsx
@@ -9,7 +9,18 @@ import { Button } from "antd";
 export default function Player() {
   return (
     <>
-    <div className=" w-full h-24 bg-cyan-100 dark:bg-cyan-900">播放导航栏</div>
+      <div className=" w-full h-24 bg-cyan-100 dark:bg-cyan-900 flex items-center pl-3 pr-3">
+        <div className="flex w-64 h-full items-center">
+          <img
+            className=" w-20 h-20 rounded-xl"
+            src="https://picsum.photos/200"
+          />
+          <div className=" flex-1  pl-3">
+            <h3 className="w-full ">歌名</h3>
+            <p className="w-full">作者</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
